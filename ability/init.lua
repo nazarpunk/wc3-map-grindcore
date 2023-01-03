810cca0b96300f2d2752c4f67a2410197e7893f7
nazarpunk <nazarpunk@gmail.com>
2023-01-03 03:48:00 +0200

    spell

--- ability/init.lua
+++ ability/init.lua
@@ -6,7 +6,7 @@ EVENT_PLAYER_MOUSE_MOVE = ConvertPlayerEvent(307) ---@type playerevent
 
 local trig = CreateTrigger()
 for i = 0, bj_MAX_PLAYER_SLOTS - 1, 1 do
-    local p = Player(0)
+    local p = Player(i)
     PlayerMouse[i] = { 0, 0, 0 }
     TriggerRegisterPlayerEvent(trig, p, EVENT_PLAYER_MOUSE_MOVE)
 end
